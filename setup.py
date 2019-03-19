2a1df3f3cdd4f40518ccacaa91ce3abbfa1ac7d0
Thembisile Damon <Thembisile@explore-datascience.net>
2019-03-19 15:25:05 +0200

    added init file for importing functions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mypisco',
-    version='0.1',
+    version='0.5',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Python package for recursion and sorting functionality',
