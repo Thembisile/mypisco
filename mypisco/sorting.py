293992d1c947e74d5c51b9b951717adab09e502f
Thembisile Damon <Thembisile@explore-datascience.net>
2019-03-19 13:06:31 +0200

    initial commit on project

--- mypisco/sorting.py
+++ mypisco/sorting.py
@@ -7,36 +7,38 @@ def bubble_sort(items):
                items[i+1] = temp
    return items
 print(bubble_sort([8,5,46,16,22,18]))
+
 def merge_sort(items):
-    len_i = len(items)
-    # Base case. A list of size 1 is sorted.
-    # Cae returns, so if reached then function will terminate after line 8
-    if len_i == 1:
-        return items
-    # Recursive case. Find midpoint of list
-    mid_point = int(len_i / 2)
-    # divide list into two halves
-    i1 = merge_sort(items[:mid_point])
-    i2 = merge_sort(items[mid_point:])
-    # call merge_sort function on each half of list 
-    return merge(i1, i2)
-# Merge function.
-def merge(A, B):
-    new_list = []
-    while len(A) > 0 and len(B) > 0:
-        if A[0] < B[0]:
-            new_list.append(A[0])
-            A.pop(0)
-        else:
-            new_list.append(B[0])
-            B.pop(0)
+    def merge_sort(items):
+        len_i = len(items)
 
-    if len(A) == 0:
-        new_list = new_list + B
-    if len(B) == 0:
-        new_list = new_list + A
+        if len_i == 1:
+            return items
+ 
+        mid_point = int(len_i / 2)
+        
+        i1 = merge_sort(items[:mid_point])
+        i2 = merge_sort(items[mid_point:])
 
-    return new_list
+        return merge(i1, i2)
+
+    def merge(A, B):
+        new_list = []
+        while len(A) > 0 and len(B) > 0:
+            if A[0] < B[0]:
+                new_list.append(A[0])
+                A.pop(0)
+            else:
+                new_list.append(B[0])
+                B.pop(0)
+
+        if len(A) == 0:
+            new_list = new_list + B
+        if len(B) == 0:
+            new_list = new_list + A
+
+        return new_list
+    return merge_sort(items)
 print(merge_sort([8,5,46,16,22,18]))
 
 def quick_sort(items):
